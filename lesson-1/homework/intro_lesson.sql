3204e9f2610556c3427bd00fb90e9092d1c54402
Moonlight0027 <qadamboyevs27@gmail.com>
2025-05-05 13:20:16 +0500

    Create intro_lesson.sql

---
+++ lesson-1/homework/intro_lesson.sql
@@ -0,0 +1,14 @@
+--lesson 3 constraints and files
+
+-- task 1
+--Define and explain the purpose of BULK INSERT in SQL Server.
+
+dknsonvsdnvsdv
+
+--task 2
+dmpsdmvsd
+
+
+
+--task 25 
+select * from table
